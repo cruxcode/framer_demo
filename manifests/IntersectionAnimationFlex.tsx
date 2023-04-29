65f26454fba519f0e7835a52d0a07d1f27482656
cruxcode <swaroopshyam0@gmail.com>
2023-04-28 23:24:18 -0700

    intersection observer setup complete

--- manifests/IntersectionAnimationFlex.tsx
+++ manifests/IntersectionAnimationFlex.tsx
@@ -1,11 +1,39 @@
-import React from "react";
+import React, { useEffect, useMemo, useImperativeHandle, useRef } from "react";
 
 const IntersectionAnimationFlex = React.forwardRef<
 	HTMLDivElement,
 	{ children: React.ReactNode[]; styles: React.CSSProperties }
 >((props, ref) => {
+	const internalRef = useRef<HTMLDivElement>(null);
+
+	const observer = useMemo(() => {
+		return new IntersectionObserver(
+			(entries) => {
+				console.log(entries);
+				entries.forEach((entry) => {
+					if (entry.isIntersecting) {
+						// animation start
+					}
+				});
+				console.log("intersection callback called");
+			},
+			{ threshold: 0.4 }
+		);
+	}, []);
+
+	useEffect(() => {
+		if (internalRef && internalRef.current) {
+			observer.observe(internalRef.current);
+		}
+	}, []);
+
+	// @ts-ignore
+	useImperativeHandle(ref, () => {
+		return internalRef.current;
+	});
+
 	return (
-		<div ref={ref} style={{ display: "flex", ...props.styles }}>
+		<div ref={internalRef} style={{ display: "flex", ...props.styles }}>
 			{props.children}
 		</div>
 	);
