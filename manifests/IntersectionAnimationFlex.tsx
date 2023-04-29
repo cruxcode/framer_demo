6e15e2f54025b526376c9941e5d049c1a4160ab3
cruxcode <swaroopshyam0@gmail.com>
2023-04-28 23:30:54 -0700

    animation done

--- manifests/IntersectionAnimationFlex.tsx
+++ manifests/IntersectionAnimationFlex.tsx
@@ -1,10 +1,18 @@
 import React, { useEffect, useMemo, useImperativeHandle, useRef } from "react";
+import { motion, useAnimationControls } from "framer-motion";
 
 const IntersectionAnimationFlex = React.forwardRef<
 	HTMLDivElement,
 	{ children: React.ReactNode[]; styles: React.CSSProperties }
 >((props, ref) => {
 	const internalRef = useRef<HTMLDivElement>(null);
+
+	// animation
+	const controls = useAnimationControls();
+	const variations = {
+		initial: { opacity: 0, top: 32 },
+		inView: { opacity: 1, top: 0 },
+	};
 
 	const observer = useMemo(() => {
 		return new IntersectionObserver(
@@ -13,13 +21,13 @@ const IntersectionAnimationFlex = React.forwardRef<
 				entries.forEach((entry) => {
 					if (entry.isIntersecting) {
 						// animation start
+						controls.start("inView");
 					}
 				});
-				console.log("intersection callback called");
 			},
 			{ threshold: 0.4 }
 		);
-	}, []);
+	}, [controls]);
 
 	useEffect(() => {
 		if (internalRef && internalRef.current) {
@@ -33,9 +41,15 @@ const IntersectionAnimationFlex = React.forwardRef<
 	});
 
 	return (
-		<div ref={internalRef} style={{ display: "flex", ...props.styles }}>
+		<motion.div
+			ref={internalRef}
+			style={{ ...props.styles, display: "flex", position: "relative" }}
+			variants={variations}
+			initial="initial"
+			animate={controls}
+		>
 			{props.children}
-		</div>
+		</motion.div>
 	);
 });
 
